84ca1554d29be43ef49ea841740d867b492c8a86
abikshaw <abikshaw24@gmail.com>
2026-08-28 18:04:44 +0530

    Fix Checkout import case

--- lavsfe/src/App.jsx
+++ lavsfe/src/App.jsx
@@ -10,7 +10,7 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Products from "./Pages/Products";
 import Contact from "./Pages/Contact";
-import CheckOut from "./Pages/checkout";
+import CheckOut from "./Pages/CheckingOut";
 import Cart from "./Pages/Cart";
 import Billing from "./Pages/Billing";
 import Register from "./Pages/Register";
