5bc10458aa33882f1908f2b38945fd7c2223f797
abikshaw <abikshaw24@gmail.com>
2026-09-24 13:09:25 +0530

    Fix API base URL for products

--- lavsfe/src/Services/Api.js
+++ lavsfe/src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://grocery-shop-app-mfdq.onrender.com"
+  baseURL: "https://grocery-shop-app-mfdq.onrender.com/api"
 });
 
 export default API;
